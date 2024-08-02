8fdb8fef9d2d2f35d7048e592b3768e51a155cd0
djmnason <daniel.j.m.nason@gmail.com>
2024-08-02 17:27:01 -0400

    Commit data, modeling, and game folders with data

---
+++ game/dond.py
@@ -0,0 +1,85 @@
+from dond_utils import *
+import os, time
+
+# # Start the game
+def play_dond():
+    os.system('cls')
+    print("Welcome!\n\nLet's play Deal or No Deal!")
+    case_values = [
+        0.01, 1.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0,
+        200.0, 300.0, 400.0, 500.0, 750.0, 1000.0, 5000.0,
+        10000.0, 25000.0, 50000.0, 75000.0, 100000.0,
+        200000.0, 300000.0, 400000.0, 500000.0, 750000.0, 1000000.0,
+    ]
+    random.shuffle(case_values)
+    cases = {i+1 : case_values[i] for i in range(len(case_values))}
+
+    turns = [6,5,4,3,2,1,1,1,1]
+    round_offer_accepted = None
+    stay_or_switch = None
+    other_choice = None
+    offers_tracker = {}
+
+    player_case_number = select_valid_case(cases=cases)
+    print(f'You chose case number: {player_case_number}')
+    player_case_value = cases.pop(player_case_number)
+
+    for current_round, round_number in enumerate(range(len(turns)), start=1):
+        print_round_start(current_round, cases, case_values)
+        for _ in range(turns[round_number]):
+            current_turn_case = select_valid_case(cases)
+            eliminated_value = cases.pop(current_turn_case)
+            case_values.pop(case_values.index(eliminated_value))
+            print(f"Case #{current_turn_case} contained ${eliminated_value:.2f}")
+        
+        ## calculate offer
+        current_offer = calculate_banker_offer(current_round, cases)
+        offers_tracker[current_round] = current_offer
+        print(f"The banker's offer for round {current_round} is ${current_offer:.2f}.")
+        time.sleep(1)
+
+        ## if player doesn't have deal already
+        if round_offer_accepted:
+            pass
+
+        else:
+            ## ask player to take the offer
+            player_decision = validate_player_decision(prompt="Deal or No Deal? (deal/no deal):", decisions=['no deal', 'deal'])
+            
+            if player_decision == 'deal':
+                player_winnings = current_offer
+                round_offer_accepted = current_round
+                print('You took the deal!')
+                if current_round < 9:
+                    print("Let's see what would have happened if you had not accepted the deal.")
+                    time.sleep(1)
+        
+
+        ## add what-if logic
+        os.system('cls')
+    else:
+        if round_offer_accepted:
+            pass
+        else:
+            print(cases)
+            stay_or_switch = validate_player_decision(
+                prompt='No more rounds left.\nRemaining values:\n{}\nStick with your original case? (y/n):'.format(', '.join(str(c) for c in sorted(case_values))),
+                decisions=['y','n']
+            )
+            last_case = cases.pop(list(cases.keys())[0])
+            if stay_or_switch == 'y':
+                player_winnings, other_choice = player_case_value, last_case
+            else:
+                player_winnings, other_choice = last_case, player_case_value
+            offers_tracker[current_round + 1] = other_choice
+    
+    game_summary_message(
+        player_winnings=player_winnings,
+        offers_tracker=offers_tracker,
+        player_case_number=player_case_number,
+        player_case_value=player_case_value,
+        round_offer_accepted=round_offer_accepted
+    )
+
+if __name__ == '__main__':
+    play_dond()
---
+++ game/dond_utils.py
@@ -0,0 +1,109 @@
+import pickle
+import random
+
+with open('dond_dict_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+def select_valid_case(cases):
+    while True:
+        try:
+            selection = int(input("Choose your case number (1-26): "))
+            if selection in cases:
+                return selection
+        except:
+            pass
+        print('Invalid entry, input must be an available case number. Try again.')
+
+def validate_player_decision(prompt, decisions):
+    while True:
+        player_decision = input(f"{prompt}: ").strip().lower()
+        if player_decision in decisions:
+            return player_decision
+        print(f'Invalid input, please enter one of the following: {"/".join(decisions)}')
+
+def deal_message_generator(
+    player_winnings,
+    offers_tracker,
+    player_case_value,
+):
+    if player_winnings > player_case_value:
+        if player_winnings > max(list(offers_tracker.values())):
+            message = 'Congratulations, you made the best deal!'
+        else:
+            message = 'You made a good deal!'
+    else:
+        message = 'Sorry, you did not make a good deal.'
+    return message
+
+## final message summary
+def game_summary_message(
+        player_winnings, 
+        offers_tracker, 
+        player_case_number,
+        player_case_value, 
+        round_offer_accepted=None
+):
+    deal_message = deal_message_generator(player_winnings, offers_tracker, player_case_value)
+    print(f"Game recap:\nYour case: {player_case_number} had a value of ${player_case_value:.2f}")
+    for round_, offer in offers_tracker.items():
+        if round_ == round_offer_accepted:
+            accepted_message = 'Yes'
+        elif round_ > round_offer_accepted:
+            accepted_message = 'Already accepted'
+        else:
+            accepted_message = 'No'
+        print(f"Round {round_} offer of ${offer:.2f}. Offer accepted?: {accepted_message}")
+    print(f'{deal_message}\nYour winnings: ${player_winnings:.2f}\n\nThank you for playing Deal or No Deal!')
+
+def print_round_start(round_number, cases, case_values):
+    print(f'Round number {round_number}\n')
+    print('Available cases:\n{}'.format(', '.join(str(c) for c in cases)))
+    print('Values remaining\n{}'.format(', '.join(str(c) for c in sorted(case_values))))
+
+def find_quantile(value, seq):
+    seq = sorted(seq)
+    if value not in seq:
+        seq.append(value)
+        seq.sort()
+    return seq.index(value) / (len(seq) - 1)
+
+def find_quantile_index(qtile, seq):
+    if 0 <= qtile <= 1:
+        return seq.index(seq[int(qtile * (len(seq) - 1))])
+    else:
+        raise ValueError(f'Quantile expected to be between 0 and 1, received {qtile}.')
+
+def validate_index_bounds(ind, bound):
+    return min(0, ind-bound), ind+bound
+
+def get_quantile_range(remaining_avg, round_number, model_dict):
+    ## get value associated with current quantile
+    dist_qtile = find_quantile(remaining_avg, model_dict[round_number].get('remaining_avg'))
+    dist_qtile_index = find_quantile_index(dist_qtile, model_dict[round_number].get('remaining_avg'))
+
+    ## get +/- 1 decile from current quantile
+    qtile_bound = find_quantile_index(0.1, model_dict[round_number].get('remaining_avg'))
+    lower_bound, upper_bound = validate_index_bounds(dist_qtile_index, qtile_bound)
+
+    ## slice sample range from data and return data
+    return model_dict[round_number].get('offer_avg_ratio')[lower_bound:upper_bound]
+
+def calculate_banker_offer(round_number, cases,  model_dict=model, n_sims=30):
+    ## calculate current average
+    remaining_avg = sum(cases.values()) / len(cases)
+
+    ## get slice of distribution based on current values
+    qtile_range = get_quantile_range(remaining_avg, round_number, model_dict)
+
+    ## bootstrap results and generate offer
+    bootstrapped_offer_ratio = sum(random.choices(qtile_range, k=n_sims)) / n_sims
+
+    return remaining_avg * bootstrapped_offer_ratio
+
+# def calculate_banker_offer(round_number, cases):
+#     return model.predict(
+#         {
+#             'round' : round_number,
+#             'remaining_avg' : sum(cases.values()) / len(cases)
+#         }
+#     ).iloc[0]
